db99826456680d36d9c5924875d00b2a912c5477
Pankaj Rawat <pankaj.rawat01@nagarro.com>
2025-02-21 13:54:34 +0530

    Changes to return user details on sign up

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -21,8 +21,14 @@ authRouter.post("/signup", async (req, res) => {
             emailId,
             password: passwordHash
         });
-        await user.save();
-        res.send("User added successfully");
+        const savedUser = await user.save();
+        const token = await savedUser.getJWT();
+
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + 8* 3600000)
+        });
+
+        res.json({message: "User added successfully", data: savedUser});
     } catch (err) {
         res.status(400).send("Error :" + err.message);
     }
@@ -43,7 +49,9 @@ authRouter.post("/login", async (req, res) => {
 
             const token = await user.getJWT();
 
-            res.cookie("token", token);
+            res.cookie("token", token, {
+                expires: new Date(Date.now() + 8* 3600000)
+            });
             res.send(user);
         } else {
             throw new Error('Invalid credentials');
